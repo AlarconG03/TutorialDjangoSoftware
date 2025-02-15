86026e916d5239540e27142b50d015879b492770
Samuel Alarcón <samuelito520@outlook.com>
2025-02-15 12:46:09 -0500

    Commit 5B 2.0

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView
+from pages.utils import ImageLocalStorage
+from .views import HomePageView, AboutPageView, ContactPageView, ImageViewNoDI, ProductIndexView
 from .views import ProductShowView, ProductCreateView, CartView, CartRemoveAllView
+from .views import ImageViewFactory
+
 
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
@@ -11,5 +14,9 @@ urlpatterns = [
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
-    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll')
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'), 
 ]
